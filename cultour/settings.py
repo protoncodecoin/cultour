b3709d18a5110eed38b8ce3889deda8f73f5841a
protoncodecoin <princeaffumasante@gmail.com>
2025-08-10 20:29:43 +0000

    change secret key

--- cultour/settings.py
+++ cultour/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-k_q2ezmgzm)-i*pq(q-4$0o@rmxwqd7we1t750+bi1e+r3z%p8"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["10.10.60.55", "*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition

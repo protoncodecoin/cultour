ff42dee67cec0024e9a63def03674c97f5ad4d58
protoncodecoin <princeaffumasante@gmail.com>
2025-08-12 06:11:39 +0000

    update and bug fixes

--- hotels/admin.py
+++ hotels/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from hotels.models import Hotel, HotelReservation, HotelRoom
+from hotels.models import AvailabilityRequest, Hotel, HotelReservation, HotelRoom
 
 
 # Register your models here.
@@ -47,3 +47,18 @@ class HotelReservationAdmin(admin.ModelAdmin):
     ]
     list_filter = ["check_in", "check_out", "status", "created_on", "updated_on"]
     search_fields = ["special_requests"]
+
+
+@admin.register(AvailabilityRequest)
+class AvailabilityRequestAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "email",
+        "date_from",
+        "date_to",
+        "guests",
+        "children",
+        "created_at",
+        "hotel",
+    ]
+    list_filter = ["date_from", "date_to", "created_at"]
